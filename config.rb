833f47faebc8fc172fb34e7720d7e3aaa0b13a40
Jonathan Lo <jon.lo@berkeley.edu>
2015-08-16 13:32:23 -0700

    adding CNAME redirect

--- config.rb
+++ config.rb
@@ -78,7 +78,9 @@ end
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-activate :livereload
+configure :development do
+  activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
